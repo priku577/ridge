1369f6bb742eebe9b8d1653bcf58db22ff343686
priku577 <priyapullolickal@gmail.com>
2017-11-09 14:28:35 +0100

    commiting the vignette

--- ridge/vignettes/vig.R
+++ ridge/vignettes/vig.R
@@ -5,227 +5,90 @@ knitr::opts_chunk$set(
 )
 
 ## ------------------------------------------------------------------------
+library(ridge)
 library(mlbench)
-library(caret)
-require(ridge)
-library(methods)
+library(caret) 
+library(leaps)
 data("BostonHousing")
 
-## ---- echo=FALSE, eval=TRUE----------------------------------------------
-ridgereg <- setRefClass("ridgereg", 
-                        fields = list(formula="formula",
-                                      data="data.frame",
-                                      reg_coe="matrix",
-                                      fit_val="matrix",
-                                      data_name="character",
-                                      lambda="numeric"),
-                        
-                        methods = list(
-                          initialize = function(formula, data, lambda=0){
-                            
-                            formula <<- formula
-                            data <<- data
-                            lambda <<- lambda
-                            
-                            data_name <<- deparse(substitute(data))
-                            
-                            #getting y variable from the formula
-                            get_y <- all.vars(formula)[1]
-                            
-                            #defining X and y
-                            y <- data[[get_y]]
-                            X <- model.matrix(formula, data)
-                            
-                            #Normalize
-                            p <- ncol(X)
-                            X <- cbind(X[,1], scale(X[,-1]))
-                            
-                            #Ridge regression
-                            # reg_coe <<- solve(t(X)%*%X+lambda*diag(p)) %*% t(X)%*%y
-                            # dimnames(reg_coe) <<- list(c("(Intercept)", all.vars(formula)[-1]), NULL)
+## ------------------------------------------------------------------------
+data("BostonHousing")
+names(BostonHousing)
+train_index <- caret::createDataPartition(BostonHousing$age, p = .75,
+                                         list = FALSE,
+                                         times= 1)
+train_data <- BostonHousing[train_index, ]
+test_data <- BostonHousing[-train_index, ]
 
-                            # fit_val <<- X%*%reg_coe
-                            
-                            #QR Decomposition
-                            y<-as.matrix(data[get_y],ncol=1)
-                            X_qr<-rbind(X,sqrt(lambda)*diag(p))
-                            y_qr<-rbind(y,matrix(data = 0, nrow = p, ncol = 1))
-                            
-                            QR <- qr((X_qr))
-                            Q <- qr.Q(QR)
-                            R <- qr.R(QR)
-                            
-                            reg_coe <<- qr.solve(R) %*% t(Q) %*% as.matrix(y_qr)
-                            dimnames(reg_coe) <<- list(c("(Intercept)", all.vars(formula)[-1]), NULL)
-                            
-                            fit_val <<- X %*% reg_coe
-                          },
-                          
-                          results = function(){
-                            results <- list()
-                            
-                            results$coef <- reg_coe
-                            results$fitted <- fit_val
-                            return(results)
-                            
-                          },
-                          
-                          print = function(){
-                            "Print out the coefficients and coefficient names"
-                            
-                            cat("Call: \n ")
-                            cat(paste0("ridgereg(formula = ",format(formula),", data = ",data_name,", lambda = ",lambda,")\n\n"))
-                            cat("Coefficients: \n")
-                            cat(" ",row.names(reg_coe), "\n    ", sep = "  ")
-                            cat(t(reg_coe[,1]), sep="    ")
-                            
-                          },
-                          
-                          
-                          predict = function(newdata=NULL){ #if newdata is used, it should be a data frame
-                            "Prints out the predicted values or if newdata is used, prints out predicted values for the new data set"
-                            
-                            if(is.null(newdata)){
-                              result <- structure(c(results()[[2]]), names=(1:length(results()[[2]])))
-                            } else{
-                              X<-model.matrix(object=formula, data=newdata)
-                              X[,2:ncol(X)] <- scale(X[,-1])
-                              result <- (X %*% results()[[1]])[,1]
-                            }
-                            return(result)
-                          },
-                          
-                          # predict = function(){
-                          #   "Return the predicted values y_hat"
-                          #   return(fit_val)
-                          # },
-                          
-                          coef = function(){
-                            "Return the coefficients as a named vector"
-                            return(reg_coe)
-                          }
-                        )
+
+head(train_data)
+head(test_data)
+
+## ------------------------------------------------------------------------
+ridge <- caret::train(crim~.,
+                      data = train_data,
+                      method='lm',
+                      trControl = trainControl(method = "cv")
 )
 
-## ------------------------------------------------------------------------
-head(BostonHousing)
+print(ridge)
 
 ## ------------------------------------------------------------------------
-set.seed(12345)
-inTrain <- createDataPartition(BostonHousing$medv, p = .7, list = FALSE)
-training <- BostonHousing[ inTrain,]
-testing  <- BostonHousing[-inTrain,]
+lflmGrid <- expand.grid(nvmax=1:(ncol(train_data)-1))
 
-#For the 10-fold cross-validation
-ctrl <- caret::trainControl(method = "repeatedcv",
-  number = 10, # k=10
-  repeats = 10) # repeat 10 times
-
-## ------------------------------------------------------------------------
-BostonHousing$chas <- as.numeric(BostonHousing$chas) - 1
-BostonHousing <- scale(BostonHousing)
+ridge <- caret::train(crim~.,
+                      data = train_data,
+                      method='leapForward',
+                      tuneGrid = lflmGrid
+)
+print(ridge)
 
 ## ------------------------------------------------------------------------
-set.seed(12345)
-lm <- caret::train(medv ~ ., 
-            data = training,
-            method = "lm",
-            trControl = ctrl)
-lm
+ridge <- list(type="Regression", 
+              library="ridge",
+              loop=NULL,
+              prob=NULL)
 
-## ------------------------------------------------------------------------
-set.seed(12345)
-lflmGrid <- expand.grid(nvmax=1:(ncol(training)-1))
-lflm <- caret::train(medv ~ ., 
-              data = training,
-              method = "leapForward",
-              trControl = ctrl,
-              tuneGrid = lflmGrid)
+ridge$parameters <- data.frame(parameter="lambda",
+                               class="numeric",
+                               label="lambda")
 
-lflm
 
-summary(lflm)
+ridge$grid <- function (x, y, len = NULL, search = "grid"){
+  data.frame(lambda = lambda)
+} 
 
-## ---- fig.height= 7, fig.width= 7----------------------------------------
-p_data <- as.data.frame(cbind(predict(lflm), scale(resid(lflm))))
-
-plot1 <- ggplot(p_data, aes(x = p_data[,1], y = p_data[,2])) + geom_point() +
-          geom_smooth(method = "loess", color = "red") + xlab("Fitted") +
-          ylab("Residuals") + ggtitle("Resid vs Fitted with 11 factors") +
-          theme(plot.title = element_text(hjust = 0.5))
-
-plot(plot1)
-
-## ---- warning=FALSE------------------------------------------------------
-#To use rigdereg() function
-rglm <- list(type=c("Classification", "Regression"),
-            library="ridge",
-            loop=NULL,
-            prob=NULL)
-
-#Parameter setting
-rglm$label<-"Ridge regression by mlr package"
-
-rglm$parameters <- data.frame(parameter = "lambda",
-                  class = "numeric",
-                  label = "lambda")
-
-rglm$fit <- function(x, y, lambda, param, lev, last, classProbs, ...) { 
-  dat <- as.data.frame(x)
+ridge$fit <- function (x, y, wts, param, lev, last, classProbs, ...) {
+  dat <- if (is.data.frame(x)) 
+    x
+  else as.data.frame(x)
+  dat$.outcome <- y
+  out <- ridgereg$new(.outcome ~ ., data=dat ,lambda = param$lambda, normalize=normalize, ...)
   
-  respvector <- NULL
-  respname <- NULL
-  respnum <- NULL
+  out
+}
 
-  for(i in 1:ncol(x)){
-    if(identical(y,dat[,i])){
-    respvector <- dat[,i]
-    respname <- names(x)[i]
-    respnum <- i
-    }
-  }
-
-  formula <- paste(respname,"~", sep="")
-
-  if(ncol(x) > 1){
-    for(i in 1:ncol(x)-1){
-            formula <- paste(formula, "+", names(dat)[i], sep="")
-    }
-  }
-
-  formula <- as.formula(formula)
-  
-  ridgereg$new(formula = formula, data=dat,lambda= param$lambda)
- }
- 
-rglm$predict <- function(modelFit, newdata, preProc=NULL, submodels = NULL){
+ridge$predict <- function (modelFit, newdata, submodels = NULL) {
   if (!is.data.frame(newdata)) 
     newdata <- as.data.frame(newdata)
+  newdata <- scale(newdata)
   modelFit$predict(newdata)
 }
 
-rglm$sort  <- function(x) x[order(-x$lambda),]
-
-
-rglm$grid <- function(x, y, len=NULL, search="grid"){
-  data.frame(lambda=seq(from=0, to=5, by=1))
-}
-
-set.seed(12345)
-rglm_fit <- caret::train(x = training,
-                  y = training$medv,
-                  method = rglm,
-                  trControl = ctrl)
-
-rglm_fit
-
 ## ------------------------------------------------------------------------
-#Linear regression
-lm$results[,1:4]
-
-#Regression with forward selection
-lflm$results[11,1:4]
-
-#Ridge regression
-rglm_fit$results[4,1:4]
+library(MASS)
+fitControl <- caret::trainControl(method = "cv",
+                                  number = 10)
+lambdaGrid <- expand.grid(lambda = c(0,.01,.02,.03,.04))
+ridge <- caret::train(crim~.,
+                      data = train_data,
+                      method='ridge',
+                      trControl = fitControl,
+                      tuneGrid = lambdaGrid,
+                      preProcess=c('center', 'scale')
+)
+predict(ridge$finalModel, type='coef', mode='norm')$coefficients[13,]
+ridge.pred <- predict(ridge, test_data)
+avgErrror<-2*sqrt(mean(ridge.pred - test_data$crim)^2)
+print(ridge)
 
